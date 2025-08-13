d9e548456da57efef56f4b7c8ffb8016e5b6cfca
matheussOliveira1 <matheus.aoliveira@sptech.school>
2025-08-13 13:50:17 -0300

    feat: adicionando geracao de token jwt e autenticacao para chamadas dos endpoints apenas com token valido

--- src/main/java/med/voll/api/infrastructure/exception/MedicoException.java
+++ src/main/java/med/voll/api/infrastructure/exception/MedicoException.java
@@ -2,7 +2,12 @@ package med.voll.api.infrastructure.exception;
 
 import jakarta.persistence.EntityNotFoundException;
 import med.voll.api.dto.medico.DadosMedicoDTO;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -24,16 +29,39 @@ public class MedicoException {
                 .map(DadosErroValidacaoDTO::new).toList());
     }
 
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity tratarErro400(HttpMessageNotReadableException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity tratarErroBadCredentials() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciais inválidas");
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity tratarErroAuthentication() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Falha na autenticação");
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity tratarErroAcessoNegado() {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Acesso negado");
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity tratarErro500(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro: " +ex.getLocalizedMessage());
+    }
+
     private record DadosErroValidacaoDTO(
             String campo,
             String mensagem
     )
     {
-
         public DadosErroValidacaoDTO(FieldError err){
             this(err.getField(), err.getDefaultMessage());
         }
-
     }
 
 }
